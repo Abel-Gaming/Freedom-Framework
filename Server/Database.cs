d86093e8051511bda4e9830fcdc53e26524deeab
Abel Gaming <lcpdpolice4939@gmail.com>
2020-12-30 19:53:40 -0500

    Create Database.cs

---
+++ Server/Database.cs
@@ -0,0 +1,57 @@
+using MySqlConnector;
+using System.Data;
+
+namespace server
+{
+    public class Database
+    {
+        public static MySqlConnection Connection;
+
+        public static void Initialize()
+        {
+            MySqlConnectionStringBuilder Builder = new MySqlConnectionStringBuilder
+            {
+                Server = Config.Host,
+                Port = Config.Port,
+                UserID = Config.User,
+                Password = Config.Password,
+                Database = Config.Database
+            };
+
+            Connection = new MySqlConnection(Builder.ToString());
+        }
+
+        public static MySqlDataReader ExecuteSelectQuery(string Sql)
+        {
+            MySqlCommand Command = new MySqlCommand(Sql, Connection);
+
+            if (Connection.State == ConnectionState.Closed)
+            {
+                Connection.Open();
+            }
+
+            MySqlDataReader Result = Command.ExecuteReader();
+
+            return Result;
+        }
+
+        public static void ExecuteInsertQuery(string Sql)
+        {
+            MySqlCommand Command = new MySqlCommand(Sql, Connection);
+
+            if (Connection.State == ConnectionState.Closed)
+            {
+                Connection.Open();
+            }
+
+            Command.ExecuteNonQuery();
+
+            Connection.Close();
+        }
+
+        public static void ExecuteUpdateQuery(string Sql)
+        {
+            ExecuteInsertQuery(Sql);
+        }
+    }
+}
