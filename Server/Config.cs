2774bff22bcbdaedc029d74a32bf81241a3794af
Abel Gaming <lcpdpolice4939@gmail.com>
2020-12-30 19:53:57 -0500

    Create Config.cs

---
+++ Server/Config.cs
@@ -0,0 +1,11 @@
+namespace server
+{
+    public class Config
+    {
+        public const string Host = "127.0.0.1";
+        public const uint Port = 3306;
+        public const string User = "root";
+        public const string Password = "";
+        public const string Database = "freedom";
+    }
+}
