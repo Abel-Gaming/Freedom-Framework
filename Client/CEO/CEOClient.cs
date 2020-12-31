00bffb86f36d294b03c7e0a9c8ff3d013a88a20e
Abel Gaming <lcpdpolice4939@gmail.com>
2020-12-30 19:49:14 -0500

    Add files via upload

---
+++ Client/CEO/CEOClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using CitizenFX.Core.UI;
+
+namespace client.CEO
+{
+    public class CEOClient : BaseScript
+    {
+        public static bool CanRegisterCEO = false;
+        public CEOClient()
+        {
+            //Commands
+            API.RegisterCommand("organization", new Action<int, List<object>, string>((src, args, raw) =>
+            {
+                Player p = new PlayerList()[src];
+                var argList = args.Select(o => o.ToString()).ToList();
+                if (argList.Any())
+                {
+                    string displayString = string.Join(" ", argList.ToArray());
+
+                    //Trigger Server Events
+                    TriggerServerEvent("Freedom:CreateOrganization", displayString);
+                }
+            }), false);
+        }
+    }
+}
---
+++ Client/CEO/CEOEvents.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using CitizenFX.Core.UI;
+
+namespace client.CEO
+{
+    public class CEOEvents : BaseScript
+    {
+        public CEOEvents()
+        {
+            EventHandlers["Freedom:OrganizationCreated"] += new Action<string, string>(OrganizationCreated);
+        }
+
+        private static void OrganizationCreated(string displayString, string playername)
+        {
+            API.SetNotificationTextEntry("STRING");
+            API.SetNotificationColorNext(4);
+            API.AddTextComponentString($"~b~{displayString}~w~ has just been created by ~b~{playername}~w~.");
+            API.SetTextScale(0.5f, 0.5f);
+            API.SetNotificationMessage("CHAR_LIFEINVADER", "CHAR_LIFEINVADER", false, 0, "New Organization", $"{displayString}");
+            API.DrawNotification(true, false);
+        }
+    }
+}
